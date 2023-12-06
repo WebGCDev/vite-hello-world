ecb63c9f9e3ec4916b6fac6bb20c0e91896300bd
WebGCDev <gaetano.chi@gmail.com>
2023-12-06 18:25:01 +0100

    feat: AppVue create

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: AppProva,
+    data() {
+        return {
+            title: 'Quanto mi hai fatto penare'
+
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>{{ title }}</h1>
+</template>
+
+<style scoped></style>
